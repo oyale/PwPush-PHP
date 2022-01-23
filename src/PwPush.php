103b30cc4ffdba653285dad549aad0d069c842b9
Pelayo <uo231894@uniovi.es>
2022-01-23 18:22:25 +0100

    feat(PwPush): :memo: add PHPDocBlock

--- src/PwPush.php
+++ src/PwPush.php
@@ -6,6 +6,9 @@ use Exception;
 use GuzzleHttp\Exception\GuzzleException;
 use GuzzleHttp\Client;
 
+/**
+ * Push a secret to any instance of PasswordPush.
+ */
 class PwPush
 {
     private const  API_URI = "p.json";
@@ -17,19 +20,35 @@ class PwPush
     private bool   $validate = false;
     private string $JSON;
 
+    /**
+     * Private constructor
+     * @param string $payload (Required. The secret to push)
+     * @param array|null $options (An associative array with optional conf)
+     * @param string|null $urlBase (The base URL for the instance. Default: https://pwpush.com)
+     * @param bool|null $validate (Validate the JSON against the schema defined)
+     * @throws Exception
+     */
     private function __construct(string $payload, ?array $options, ?string $urlBase, ?bool $validate)
     {
         $this->payload = $payload;
         $this->options = $options ?? null;
         $this->urlBase = $urlBase ?? "https://pwpush.com";
         $this->validate = $validate ?? false;
-        $this->url = $urlBase . self::API_URI;
-        $this->JSON = $this->constructJSON($this->payload, $this->options);
+        if ($validate) {
+            $this->JSON = $this->constructJSON($this->payload, $this->options);
+        }
     }
 
+
     /**
-     * @throws Exception
+     * Static function to push a new secret to a Password Push instance
+     * @param string $payload (Required. The secret to push)
+     * @param array|null $options (An associative array with optional conf)
+     * @param string|null $urlBase (The base URL for the instance. Default: https://pwpush.com)
+     * @param bool|null $validate (Validate the JSON against the schema defined)
+     * @return string (The response from PwPush API)
      * @throws GuzzleException
+     * @throws Exception Threw when the JSON's validation fails
      */
     public static function push(string $payload, ?array $options = null, ?string $urlBase = null, ?bool $validate = false): string
     {
@@ -41,17 +60,24 @@ class PwPush
         return $PwPush->urlBase . '/p/' . $response['url_token'];
     }
 
+    /**
+     * Encode the payload + options array to JSON
+     * @throws Exception
+     */
     private function constructJSON()
     {
 
         return json_encode($this->getArray());
     }
 
+
     /**
+     * Push the secret to PwPush and returns the API response or an Exception in failure
+     * @return array
      * @throws GuzzleException
      * @throws Exception
      */
-    private function pushPassword()
+    private function pushPassword(): array
     {
         $client = new Client([
             'base_uri' => $this->urlBase,
@@ -68,8 +94,11 @@ class PwPush
         return json_decode($body, true);
     }
 
+
     /**
+     * Generates an array with payload and options to be encoded in JSON
      * @return array
+     * @throws Exception
      */
     private function getArray(): array
     {
